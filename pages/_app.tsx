d1af61d775f4bc44411e61f3ff71b5e9940b10f8
Morty2024 <153558551+Morty2024@users.noreply.github.com>
2025-04-16 21:02:22 -0500

    Fix Footer component and add syringe icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,15 @@ import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import PageTransition from "../components/PageTransition";
-import { ThemeProvider } from "../contexts/ThemeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   
   return (
-    <ThemeProvider>
-      <AnimatePresence mode="wait">
-        <PageTransition key={router.route}>
-          <Component {...pageProps} />
-        </PageTransition>
-      </AnimatePresence>
-    </ThemeProvider>
+    <AnimatePresence mode="wait">
+      <PageTransition key={router.route}>
+        <Component {...pageProps} />
+      </PageTransition>
+    </AnimatePresence>
   );
 }

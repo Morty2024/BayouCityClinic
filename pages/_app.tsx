0b0e86af20275c2425a2fbb9ffe41ad1253f0c19
Morty2024 <153558551+Morty2024@users.noreply.github.com>
2025-04-30 17:25:35 -0500

    Fix Amplify configuration format in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,11 +12,10 @@ import { disableAnalytics } from "../config/analytics";
 
 // Amplify configuration for Cognito
 const awsConfig = {
-  Auth: {
-    region: 'us-east-2',
-    userPoolId: 'us-east-2_Jsf23aKJN',
-    userPoolWebClientId: '5svs97v0pkoauc5a42vsoascrd'
-  }
+  aws_project_region: 'us-east-2',
+  aws_cognito_region: 'us-east-2',
+  aws_user_pools_id: 'us-east-2_Jsf23aKJN',
+  aws_user_pools_web_client_id: '5svs97v0pkoauc5a42vsoascrd',
 };
 
 // Cognito OIDC configuration
